c7e08838f4af3a632c9113a303ad07f31641043e
cngo502 <32520903+cngo502@users.noreply.github.com>
2018-03-03 22:08:30 -0600

    Add project files.

---
+++ HACKT03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HACKT03.Startup))]
+namespace HACKT03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
