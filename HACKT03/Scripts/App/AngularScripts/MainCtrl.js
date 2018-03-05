884be219fdcaa9170185f9c6930477750d4ae5e8
cngo502 <32520903+cngo502@users.noreply.github.com>
2018-03-04 21:55:34 -0600

    s

--- HACKT03/Scripts/App/AngularScripts/MainCtrl.js
+++ HACKT03/Scripts/App/AngularScripts/MainCtrl.js
@@ -121,7 +121,7 @@
 					searchLayer.add(graphic);
 					console.log("search graphic layer is: ", searchLayer, search);
 
-					//routeParams.stops.features.push(searchLayer);
+					routeParams.stops.features.push(searchLayer);
 
 					var zoomExtent = esri.graphicsExtent(searchLayer.graphics);
 					console.log("zoom extent is: ", zoomExtent);
@@ -148,10 +148,11 @@
 						console.log("location is: ", response);
 						doSearchGeo(response.data, 'content/images/supplierlocation.png');
 						//routeParams.stops.features.push(searchLayer);
-						//if (routeParams.stops.features.length >= 2) {
-						//	routeTask.solve(routeParams);
-						//	lastStop = routeParams.stops.features.splice(0, 1)[0];
-						//}
+						console.log("routeParams is: ", routeParams);
+						if (routeParams.stops.features.length >= 2) {
+							routeTask.solve(routeParams);
+							lastStop = routeParams.stops.features.splice(0, 1)[0];
+						}
 
 					}, function () {
 					});
@@ -186,24 +187,24 @@
 
 
 				////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-				//var map, routeTask, routeParams;
-				//var stopSymbol, routeSymbol, lastStop;
-				//routeTask = new RouteTask("https://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
+				var routeTask, routeParams;
+				var stopSymbol, routeSymbol, lastStop;
+				routeTask = new RouteTask("https://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
 
-				////setup the route parameters
-				//routeParams = new RouteParameters();
-				//routeParams.stops = new FeatureSet();
-				//routeParams.outSpatialReference = {
-				//	"wkid": 102100
-				//};
+				//setup the route parameters
+				routeParams = new RouteParameters();
+				routeParams.stops = new FeatureSet();
+				routeParams.outSpatialReference = {
+					"wkid": 4326
+				};
 
-				//routeTask.on("solve-complete", showRoute);
-				//routeTask.on("error", errorHandler);
+				routeTask.on("solve-complete", showRoute);
+				routeTask.on("error", errorHandler);
 
-				////define the symbology used to display the route
-				//stopSymbol = new SimpleMarkerSymbol().setStyle(SimpleMarkerSymbol.STYLE_CROSS).setSize(15);
-				//stopSymbol.outline.setWidth(4);
-				//routeSymbol = new SimpleLineSymbol().setColor(new dojo.Color([0, 0, 255, 0.5])).setWidth(5);
+				//define the symbology used to display the route
+				stopSymbol = new SimpleMarkerSymbol().setStyle(SimpleMarkerSymbol.STYLE_CROSS).setSize(15);
+				stopSymbol.outline.setWidth(4);
+				routeSymbol = new SimpleLineSymbol().setColor(new dojo.Color([0, 0, 255, 0.5])).setWidth(5);
 
 				////Adds a graphic when the user clicks the map. If 2 or more points exist, route is solved.
 				//function addStop(evt) {
@@ -216,18 +217,19 @@
 				//	}
 				//}
 
-				////Adds the solved route to the map as a graphic
-				//function showRoute(evt) {
-				//	map.graphics.add(evt.result.routeResults[0].route.setSymbol(routeSymbol));
-				//}
+				//Adds the solved route to the map as a graphic
+				function showRoute(evt) {
+					console.log("showRoute is: ", evt);
+					map.graphics.add(evt.result.routeResults[0].route.setSymbol(routeSymbol));
+				}
 
-				////Displays any error returned by the Route Task
-				//function errorHandler(err) {
-				//	alert("An error occured\n" + err.message + "\n" + err.details.join("\n"));
+				//Displays any error returned by the Route Task
+				function errorHandler(err) {
+					alert("An error occured\n" + err.message + "\n" + err.details.join("\n"));
 
-				//	routeParams.stops.features.splice(0, 0, lastStop);
-				//	map.graphics.remove(routeParams.stops.features.splice(1, 1)[0]);
-				//}
+					routeParams.stops.features.splice(0, 0, lastStop);
+					map.graphics.remove(routeParams.stops.features.splice(1, 1)[0]);
+				}
 
 				
 			});
